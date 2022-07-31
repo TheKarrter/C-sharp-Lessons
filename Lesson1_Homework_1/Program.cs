aea7671598edc553b9d97347d7375ffc63524e8c
Kirill <ForStmKar@gmail.com>
2022-07-31 19:50:00 +0300

    lesson1_homework_1

---
+++ Lesson1_Homework_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = -9, b = -3, min = 0, max = 0;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    min = a;
+    max = b;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
