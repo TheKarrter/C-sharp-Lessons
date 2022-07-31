0c31de06db4e2451ef496721f523b881d69ffbd8
Kirill <ForStmKar@gmail.com>
2022-07-31 20:00:50 +0300

    lesson1_homework_3

---
+++ Lesson1_Homework_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = -7;
+
+if(a % 2 == 0)
+{
+    Console.Write("Дано число ");
+    Console.WriteLine(a);
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.Write("Дано число ");
+    Console.WriteLine(a);
+    Console.WriteLine("Число нечетное");
+}
