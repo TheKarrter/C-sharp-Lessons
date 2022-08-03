c532f10bbf7d3e732246ae80c57b7b2a2790db30
Kirill <ForStmKar@gmail.com>
2022-08-03 12:10:14 +0300

    lesson3_for

---
+++ Lesson3_For/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method1(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method1(10, "abc");
+Console.WriteLine(res);
